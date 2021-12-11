9b909510e74a7bcf5e5a3b7bcda0fb2cf54d935b
fabifors <fabian.forsstrom@gmail.com>
2021-12-11 01:48:11 +0100

    Add firebase functions - add public folder

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 import express from "express";
 import routes from "./routes";
 
 const app = express();
-const PORT = 8000;
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCHWMi9mw-QVFylLavnItjqF2nvPqzfp8E",
-  authDomain: "notion-service.firebaseapp.com",
-  projectId: "notion-service",
-  storageBucket: "notion-service.appspot.com",
-  messagingSenderId: "242522295447",
-  appId: "1:242522295447:web:2bb8e7cdcdb3d507e8add2",
-};
-
-// Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
 
 /**
  * Programatically create all routes
@@ -42,6 +23,4 @@ Object.keys(routes).forEach((key) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`⚡️ [server]: Server is running at https://localhost:${PORT}`);
-});
+export default app;
